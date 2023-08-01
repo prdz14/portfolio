ba2b1b29a44ca3992191d6dfeef6ad33145dca01
amin <mihakh13@mail.ru>
2023-08-01 13:10:23 +0300

    fourth

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -41,17 +41,23 @@
 
 <script setup>
 import { ref } from 'vue';
+import Aperture from '@/assets/img/Aperture.png';
+import TourTravel from '@/assets/img/Tour-Travel.png';
+import VueCinema from '@/assets/img/King-kong.jpg';
+import Education from '@/assets/img/Education.jpg';
+import Finsweet from '@/assets/img/Finsweet.jpg';
+import Solar from '@/assets/img/Solar.jpg';
 
 const projects1 = ref([
     {
         title: 'Aperture',
-        img: require('@/assets/img/Aperture.png'),
+        img: Aperture,
         subtitle: 'Photo studios, portfolio',
         url: 'https://prdz14.github.io/Aperture/'
     },
     {
         title: 'Tour-travel',
-        img: './src/assets/img/Tour-travel.png',
+        img: TourTravel,
         subtitle: 'Travel company, portfolio',
         url: 'https://prdz14.github.io/Tour-Travel/'
     },
@@ -60,13 +66,13 @@ const projects1 = ref([
 const projects2 = ref([
     {
         title: 'Vue-cinema',
-        img: './src/assets/img/King-kong.jpg',
+        img: VueCinema,
         subtitle: 'Cinema, portfolio',
         url: 'https://prdz14.github.io/Vue/'
     },
     {
         title: 'Education',
-        img: './src/assets/img/Education.jpg',
+        img: Education,
         subtitle: 'Education, portfolio',
         url: 'https://prdz14.github.io/Education/'
     },
@@ -75,13 +81,13 @@ const projects2 = ref([
 const projects3 = ref([
     {
         title: 'Finsweet',
-        img: './src/assets/img/Finsweet.jpg',
+        img: Finsweet,
         subtitle: 'Ecology, portfolio',
         url: 'https://prdz14.github.io/Finsweet/'
     },
     {
         title: 'Solar',
-        img: './src/assets/img/Solar.jpg',
+        img: Solar,
         subtitle: 'Power station, portfolio',
         url: 'https://prdz14.github.io/SolarEX/'
     },
